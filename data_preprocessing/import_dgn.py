3d39981759393c2875c0a4883b228776a6f68519
Ciaran Byles-Ho <cbylesho@gmail.com>
2023-04-20 11:50:12 -0400

    updates to data

--- data_preprocessing/import_dgn.py
+++ data_preprocessing/import_dgn.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def ImportDGN():
     dgn = pd.read_csv("../data/raw_data/gda_disease_summary_luad.csv")
-    dgn_dict = pd.read_csv("../data/raw_data/gda_dictionary.csv")
+    dgn_dict = pd.read_csv("../data/raw_data/gda_dictionary.csv", index_col=None)
 
     score_threshold = 0.02
     ei_threshold = 0.7
@@ -11,7 +11,7 @@ def ImportDGN():
     dgn = dgn.loc[dgn['Score_gda'] >= score_threshold]
     dgn = dgn.loc[dgn['EI_gda'] > ei_threshold]
     dgn.rename({'Score_gda':'gda_score'}, axis=1, inplace=True)
-    dgn = dgn.merge(dgn_dict, on="Gene").drop(['Unnamed: 0', 'Unnamed: 0.1', 'Gene'], axis=1)
+    dgn = dgn.merge(dgn_dict, on="Gene").drop(['Gene'], axis=1)
     dgn['gda_score'] = 1
 
     return dgn[['ensembl', 'gda_score']]
